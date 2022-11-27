dd1e45a17de8336406dd4a20bfeb407675078f45
SSYino <69228648+SSYino@users.noreply.github.com>
2022-11-27 20:46:08 +0700

    Move scene_manager to config

--- Client/config/scene_manager.py
+++ Client/config/scene_manager.py
@@ -1,33 +1,32 @@
 from scenes.menu import MenuScreen
 from scenes.get_username import GetUsernameScreen
+from scenes.game import GameScreen
 
 class SceneManager:
-    def __init__(self) -> None:
-        self.scenes = [MenuScreen("menu"), GetUsernameScreen("getUsername")]
+    def __init__(self, p, net) -> None:
+        self.scenes = [MenuScreen("menu"), GetUsernameScreen("getUsername"), GameScreen("game")]
         self.current_scene = None
-        self.p = None
-
-    def start(self, p, n):
         self.p = p
-        self.net = n
-        
+        self.net = net
+
+    def start(self):
         if self.current_scene == None:
             first_scene = MenuScreen("menu")
             self.current_scene = first_scene
 
-            next_scene_name = first_scene.start(p, n)
+            next_scene_name = first_scene.start(self.p, self.net)
 
             while True:
                 found_scene = False
 
                 if not next_scene_name:
-                    p.RUN = False
+                    self.p.RUN = False
                     break
                 
                 for scene in self.scenes:
                     if scene.get_name() == next_scene_name:
                         found_scene = True
-                        next_scene_name = scene.start(p, n)
+                        next_scene_name = scene.start(self.p, self.net)
 
                 if not found_scene:
                     print("not found scene", next_scene_name)
