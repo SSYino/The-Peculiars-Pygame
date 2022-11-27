c7e30384989f205a0e40290d91f10d083090e8ee
SSYino <69228648+SSYino@users.noreply.github.com>
2022-11-27 20:48:22 +0700

    Manage display name

--- Server/commandsManager.py
+++ Server/commandsManager.py
@@ -1,4 +1,11 @@
 def manager(data):
     command = data["command"]
 
-    return {command: "test", data: "testReply"}
+    match command:
+        case "set_display_name":
+            print("Display name", data["data"])
+            return {"command": "success", "data": "set_display_name"}
+        case _:
+            print("Invalid command received in commandsManager")
+            return {"command": "Error", "data": "Invalid command"}
+
