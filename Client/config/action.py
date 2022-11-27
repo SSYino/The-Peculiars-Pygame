ce5c2408c9ef9bbea672f6ebc1121a0e3b603a15
SSYino <69228648+SSYino@users.noreply.github.com>
2022-11-27 20:45:04 +0700

    Change func to class

--- Client/config/action.py
+++ Client/config/action.py
@@ -1,3 +1,17 @@
-def receive(p, net):
-    if net.q_RECEIVE:
-        print("in action.receive", net.q_RECEIVE)
+class Receive:
+    def __init__(self, net) -> None:
+        self.net = net
+        self.run = False
+
+    def start(self):
+        self.run = True
+        q_receive = self.net.q_RECEIVE
+
+        while self.run:
+            if not q_receive.empty():
+                print(q_receive.get())
+
+
+# def receive(p, net):
+#     if net.q_RECEIVE:
+#         print("in action.receive", net.q_RECEIVE)
