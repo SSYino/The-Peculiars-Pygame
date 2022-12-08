0f397749edf0a6f9d2dc29995e1de962ad053264
SSYino <gianmario.nino@gmail.com>
2022-12-08 22:38:34 +0700

    Use sprite for buttons

--- Client/scenes/menu.py
+++ Client/scenes/menu.py
@@ -8,46 +8,55 @@ class MenuScreen(Scene):
     def start(self, p, n, m):
         self.run = True
 
-        class Button:
+        class Button(p.py.sprite.Sprite):
             def __init__(self, text, x, y, color):
+                super().__init__()
                 self.text = text
                 self.x = x
                 self.y = y
-                self.color = color
                 self.width = 250
                 self.height = 100
+                self.image = p.py.Surface((self.width, self.height))
+                self.rect = self.image.get_rect()
+                self.color = color
+                self.rect.center = (self.x, self.y)
 
-            def draw(self, win):
-                p.py.draw.rect(
-                    win, self.color, (self.x, self.y, self.width, self.height))
+            def update(self, win):
                 font = p.py.font.SysFont("comicsans", 40)
                 text = font.render(self.text, 1, (255, 255, 255))
-                win.blit(text, (self.x, self.y))
+                text_rect = text.get_rect()
+
+                text_rect.center = self.rect.center
+                win.blit(text, text_rect)
+                # win.blit(text, self.rect.midleft)
                 # win.blit(text, (self.x + round(self.width/2) - round(text.get_width()/2), self.y + round(self.height/2) - round(text.get_height()/2)))
 
             def click(self, pos):
-                x1 = pos[0]
-                y1 = pos[1]
-                if self.x <= x1 <= self.x + self.width and self.y <= y1 <= self.y + self.height:
-                    return True
-                else:
-                    return False
+                return self.rect.collidepoint(pos)
+
+        button_group = p.py.sprite.Group()
 
         while self.run:
             p.clock.tick(60)
             p.win.fill((128, 128, 128))
             # font = pygame.font.SysFont("comicsans", 60)
 
-            start_button = Button("Start Game", 500, 300, "Gray")
-            join_button = Button("Join Game", 500, 500, "Gray")
+            screen_info = p.py.display.Info()
+            current_w, current_h = screen_info.current_w, screen_info.current_h
+
+            start_button = Button("Start Game", (current_w)/2, (current_h)/2 - 100, "Gray")
+            join_button = Button("Join Game", (current_w)/2,(current_h)/2 + 100, "Gray")
             # text = font.render("Click to Play!", 1, (255,0,0))
             # win.blit(text, (100,200))
             # win.blit(start_button, (500,300))
             # win.blit(join_button, (500,500))
-            start_button.draw(p.win)
-            join_button.draw(p.win)
+
+            button_group.add([start_button, join_button])
+            button_group.draw(p.win)
+            button_group.update(p.win)
 
             p.py.display.update()
+            button_group.empty()
 
             for event in p.py.event.get():
                 if event.type == p.py.QUIT:
