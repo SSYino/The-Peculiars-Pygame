dbd1b75fbe5cf95d8969b1e97ee8dba6937e9f06
SSYino <gianmario.nino@gmail.com>
2022-12-08 20:58:54 +0700

    Pass data manager

--- Client/config/scene_manager.py
+++ Client/config/scene_manager.py
@@ -3,18 +3,19 @@ from scenes.get_username import GetUsernameScreen
 from scenes.game import GameScreen
 
 class SceneManager:
-    def __init__(self, p, net) -> None:
+    def __init__(self, p, net, manager) -> None:
         self.scenes = [MenuScreen("menu"), GetUsernameScreen("getUsername"), GameScreen("game")]
         self.current_scene = None
         self.p = p
         self.net = net
+        self.manager = manager
 
     def start(self):
         if self.current_scene == None:
             first_scene = MenuScreen("menu")
             self.current_scene = first_scene
 
-            next_scene_name = first_scene.start(self.p, self.net)
+            next_scene_name = first_scene.start(self.p, self.net, self.manager)
 
             while True:
                 found_scene = False
@@ -26,10 +27,9 @@ class SceneManager:
                 for scene in self.scenes:
                     if scene.get_name() == next_scene_name:
                         found_scene = True
-                        next_scene_name = scene.start(self.p, self.net)
+                        next_scene_name = scene.start(self.p, self.net, self.manager)
 
                 if not found_scene:
                     print("not found scene", next_scene_name)
                     
-
 
--- Client/scenes/base_scene.py
+++ Client/scenes/base_scene.py
@@ -10,7 +10,7 @@ class Scene(ABC):
         return self.name
 
     @abstractmethod
-    def start(self, p, n):
+    def start(self, p, n, m):
         pass
 
     @abstractmethod
