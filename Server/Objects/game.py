aeccfd8e6677b5e84fd13f3db74d323914c8efdc
SSYino <gianmario.nino@gmail.com>
2022-12-08 21:04:24 +0700

    Add helper methods

--- Server/Objects/game.py
+++ Server/Objects/game.py
@@ -1,13 +1,19 @@
 class Game:
     def __init__(self, id):
         self.id = id
-        self.players = None
+        self.players = []
         self.current_location = None
         self.current_round = 0
+
+    def get_data(self):
+        return vars(self)
 
     def get_game_locations(self):
         # get game locations from a json file
         pass
+
+    def add_player(self, player):
+        self.players.append(player)
 
     def get_current_location(self):
         return self.current_location
--- Server/Objects/player.py
+++ Server/Objects/player.py
@@ -2,7 +2,10 @@ class Player:
     def __init__(self, id) -> None:
         self.id = id
         self.display_name = None
-        self.current_game_id = None
+        self.game_id = None
+
+    def get_data(self):
+        return vars(self)
 
     def set_display_name(self, name):
         self.display_name = name
