afbdff78822c8b76d6acc1033c36a684a4088337
SSYino <gianmario.nino@gmail.com>
2022-12-13 18:10:59 +0700

    Add commands
    - joinGame
    - getActiveGames

--- Server/commandsManager.py
+++ Server/commandsManager.py
@@ -14,6 +14,12 @@ def manager(data, setState):
         case "createGame":
             [isSuccess, reply] = setState(data=data)
             return Response(isSuccess, command, reply)
+        case "joinGame":
+            [isSuccess, reply] = setState(data=data)
+            return Response(isSuccess, command, reply)
+        case "getActiveGames":
+            [isSuccess, reply] = setState(data=data)
+            return Response(isSuccess, command, reply)
         case _:
             print("Invalid command received in commandsManager")
             return {"command": "Error", "data": "Invalid command"}
