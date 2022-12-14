1709f1b8b2b195fc8c6bd94b2bb1fb2f98877d26
SSYino <gianmario.nino@gmail.com>
2022-12-14 13:27:15 +0700

    Add custom command in reply

--- Server/commandsManager.py
+++ Server/commandsManager.py
@@ -1,25 +1,28 @@
 def manager(data, setState):
     command = data["command"]
 
-    def Response(isSuccess, command, data):
+    def Response(isSuccess, data, comm=command):
         if isSuccess:
-            return {"command": command, "data": data}
+            return {"command": comm, "data": data}
         else:
-            return {"command": "Unsuccessful", "data": command}
+            return {"command": "Unsuccessful", "data": comm}
 
     match command:
         case "setDisplayName":
             [isSuccess, reply] = setState(data=data)
-            return Response(isSuccess, command, reply)
+            return Response(isSuccess, reply)
         case "createGame":
             [isSuccess, reply] = setState(data=data)
-            return Response(isSuccess, command, reply)
+            return Response(isSuccess, reply)
         case "joinGame":
             [isSuccess, reply] = setState(data=data)
-            return Response(isSuccess, command, reply)
+            return Response(isSuccess, reply)
         case "getActiveGames":
             [isSuccess, reply] = setState(data=data)
-            return Response(isSuccess, command, reply)
+            return Response(isSuccess, reply)
+        case "getPlayerData":
+            [isSuccess, reply] = setState(data=data)
+            return Response(isSuccess, reply, "playerData")
         case _:
             print("Invalid command received in commandsManager")
             return {"command": "Error", "data": "Invalid command"}
