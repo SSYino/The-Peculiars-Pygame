2b0cf6ccfb05d07230bae775358750ed7a3f372d
SSYino <gianmario.nino@gmail.com>
2022-12-16 12:06:39 +0700

    Add game locations

---
+++ Client/config/game_locations.py
@@ -0,0 +1,86 @@
+LOCATIONS = [ # Currently 21 Locations
+    {
+        "name": "Airplane",
+        "roles": ["First Class Passenger", "Mechanic", "Economy Class Passenger", "Stewardess", "Co-Pilot", "Captain"]
+    },
+    {
+        "name": "Bank",
+        "roles": ["Armored Car Driver", "Manager", "Consultant", "Customer", "Robber", "Security Guard"]
+    },
+    {
+        "name": "Beach",
+        "roles": ["Lifeguard", "Beach Photographer", "Ice Cream Truck Driver", "Naked man", "Naked woman", "Drowning person", "Thief"]
+    },
+    {
+        "name": "Broadway Theater",
+        "roles": ["Cashier", "Visitor", "Director", "Actor", "Crewman"]
+    },
+    {
+        "name": "Casino",
+        "roles": ["Bartender", "Head Security Guard", "Bouncer", "Manager", "Dealer", "Gambler"]
+    },
+    {
+        "name": "Circus Tent",
+        "roles": ["Acrobat Performer", "Animal Trainer", "Magician", "Visitor", "Clown", "Juggler"]
+    },
+    {
+        "name": "Crusader Army",
+        "roles": ["Monk", "Imprisoned Arab", "Servant", "Archer", "Knight"]
+    },
+    {
+        "name": "Spa",
+        "roles": ["Customer", "Stylist", "Masseuse", "Manicurist", "Makeup Artist", "Dermatologist"]
+    },
+    {
+        "name": "Embassy",
+        "roles": ["Security Guard", "Secretary", "Ambassador", "Tourist", "Government Official"]
+    },
+    {
+        "name": "Hospital",
+        "roles": ["Nurse", "Doctor", "Intern", "Patient", "Therapist", "Surgeon"]
+    },
+    {
+        "name": "Hotel",
+        "roles": ["Doorman", "Security Guard", "Manager", "Customer", "Bartender"]
+    },
+    {
+        "name": "Movie Studio",
+        "roles": ["Stuntman", "Sound Engineer", "Cameraman", "Director", "Costume Artist", "Actor"]
+    },
+    {
+        "name": "Passenger Train",
+        "roles": ["Mechanic", "Border Patrol", "Passenger", "Restaurant Chef", "Engineer"]
+    },
+    {
+        "name": "Pirate Ship",
+        "roles": ["Cook", "Sailor", "Slave", "Cabin Boy", "Captain", "Prisoner"]
+    },
+    {
+        "name": "Police Station",
+        "roles": ["Detective", "Lawyer", "Journalist", "Archivist", "Patrol Officer", "Criminal"]
+    },
+    {
+        "name": "Restaurant",
+        "roles": ["Musician", "Customer", "Bouncer", "Head Chef", "Waiter"]
+    },
+    {
+        "name": "School",
+        "roles": ["Gym Teacher", "Student", "Principal", "Security Guard", "Janitor"]
+    },
+    {
+        "name": "Space Station",
+        "roles": ["Engineer", "Alien", "Pilot", "Commander", "Space Tourist", "Scientist", "Doctor"]
+    },
+    {
+        "name": "Submarine",
+        "roles": ["Cook", "Commander", "Sonar Technician", "Electronics Technician", "Sailor"]
+    },
+    {
+        "name": "Supermarket",
+        "roles": ["Customer", "Cashier", "Butcher", "Janitor", "Security Guard"]
+    },
+    {
+        "name": "University",
+        "roles": ["Graduate Student", "Professor", "Maintenance Man", "Dr. Visit", "Janitor"]
+    }
+]
