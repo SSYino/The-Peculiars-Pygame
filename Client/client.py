ba696bea2e1a5daea4c10780c94deafd45f363f9
SSYino <gianmario.nino@gmail.com>
2022-12-19 16:07:07 +0700

    Revert "Center and recolor text"
    This reverts commit 5681154370f470a3613c7456409cb80008d27fde.

--- Client/client.py
+++ Client/client.py
@@ -46,19 +46,7 @@ def main(p, net, data_manager):
 if __name__ == '__main__':
     p = load_pygame.Load()
     net = network.Load()
-
-    # Check for a saved display name
-    try:
-        with open("data.txt") as file:
-            data = file.read()
-            key, value = data.split("=")
-
-            data_manager = action.Receive(net, value)
-
-            
-    except Exception as e:
-        print(e)
-        data_manager = action.Receive(net)
+    data_manager = action.Receive(net)
 
     x = threading.Thread(target=data_manager.start, args=())
     y = threading.Thread(target=net.start, args=())
